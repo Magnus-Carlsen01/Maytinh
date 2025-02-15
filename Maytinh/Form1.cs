99c620fa9e23ed7218b9a4edd085800398fb2bf2
legiabao <kimngujqk@gmail.com>
2025-02-15 14:37:07 +0700

    Cap nhat form1

--- Maytinh/Form1.cs
+++ Maytinh/Form1.cs
@@ -1,10 +1,15 @@
-namespace Maytinh
+﻿namespace Maytinh
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Khởi động");
         }
     }
 }
